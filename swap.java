dac8491ad403b85b7d41fa99337c91bd2dc44f05
namburinavya26 <106248674+namburinavya26@users.noreply.github.com>
2024-04-03 17:15:05 +0530

    swap.java

---
+++ swap.java
@@ -0,0 +1,16 @@
+Write a java program to swap two numbers without using a third variable and (+ and -) operators
+  int a = 200, b = 300;
+public class Main {
+    public static void main(String[] args) {
+        int a = 200, b = 300;
+        System.out.println("Before swapping - a: " + a + ", b: " + b);
+        swapNumbersWithoutTempVariable(a, b);
+    }
+
+    public static void swapNumbersWithoutTempVariable(int a, int b) {
+        a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
+        System.out.println("After swapping - a: " + a + ", b: " + b);
+    }
+}
